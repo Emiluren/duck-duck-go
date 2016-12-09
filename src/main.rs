6661b9ae61de36d0854cf7883cf19ecd76a59141
Emil Segerbäck <emilsegerback@gmail.com>
2016-12-09 08:10:05 +0100

    Add empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate sfml;
+
+use sfml::graphics::{RenderWindow, Color, RenderTarget};
+use sfml::window::{VideoMode, ContextSettings, event, window_style};
+
 fn main() {
-    println!("Hello, world!");
+    let game_width = 800;
+    let game_height = 600;
+
+    let mut window = RenderWindow::new(
+        VideoMode::new_init(game_width, game_height, 32),
+        "Duck duck swim",
+        window_style::CLOSE,
+        &ContextSettings::default()
+    ).expect("Failed to create window.");
+
+    while window.is_open() {
+        for event in window.events() {
+            match event {
+                event::Closed => window.close(),
+                _ => {}
+            }
+        }
+        window.clear(&Color::new_rgb(0, 0, 0));
+        window.display();
+    }
 }
